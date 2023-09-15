51fb60ca26dc206e223e3199e39a9858632f43e5
AndersonPEM <sza.andersonsilva@gmail.com>
2023-09-15 10:12:15 +0100

    Added error handling and colors to be able to identify easily in the gigantic amount of logs Docker does on start.

---
+++ src/Colors/colors.go
@@ -0,0 +1,10 @@
+package Colors
+
+const (
+	RED            = "\033[91m"
+	GREEN          = "\033[92m"
+	GREENBGWHITEFG = "\033[97m\u001B[102m"
+	YELLOW         = "\033[93m"
+	RESET          = "\033[0m"
+	CYAN           = "\033[96m"
+)
--- src/main.go
+++ src/main.go
@@ -1,11 +1,33 @@
 package main
 
 import (
+	"NetKnock/Colors"
 	"fmt"
 	"net"
 	"os"
 	"time"
 )
+
+var color = ""
+
+func currentTime() string {
+	currentTime := time.Now()
+	formattedTime := currentTime.Format("02/01/2006 15:04:05")
+	return string(formattedTime)
+}
+
+func toggleColor() string {
+	if color == "" {
+		color = Colors.YELLOW
+		return color
+	}
+	if color == Colors.YELLOW {
+		color = Colors.CYAN
+		return color
+	}
+	color = Colors.YELLOW
+	return color
+}
 
 func main() {
 	// Check if the address and port are provided as an argument
@@ -16,20 +38,26 @@ func main() {
 
 	// Get the address and port from the first argument
 	address := os.Args[1]
-	fmt.Printf("Waiting for %s to be available...", address)
+	fmt.Printf("──────────────────────────────────────────────────────────────────────────────────────────\n")
+	fmt.Printf("│" + Colors.YELLOW + "  NetKnock by AndersonPEM <https://github.com/andersonpem https://gitlab.com/andersonpem>" + Colors.RESET + "│\n")
+	fmt.Printf("│" + Colors.GREEN + "One Knock to Rule Them All." + Colors.RESET + "                                                              │\n")
+	fmt.Printf("│" + Colors.GREEN + "I'll be knocking your port until it responds. And I'm very insistent. ^^" + Colors.RESET + "                 │\n")
+	fmt.Printf("│"+Colors.CYAN+"Host to be checked: %s"+Colors.RESET+"                                                       │\n", address)
+	fmt.Printf("──────────────────────────────────────────────────────────────────────────────────────────\n")
 
 	for {
 		// Attempt to connect to the address and port
 		conn, err := net.Dial("tcp", address)
 		if err != nil {
 			// Log the error if the connection fails
-			fmt.Printf("Failed to connect to %s. Error: %s\n. Will retry in a second...\n", address, err)
+			fmt.Printf(toggleColor()+"["+currentTime()+"]: I still cannot connect to %s. Error: %s.\nWill retry in a second...\n"+Colors.RESET, address, err)
 		} else {
 			// Log success and close the connection
-			fmt.Printf("Successfully connected to %s\n! We're good to go!", address)
+			fmt.Printf(Colors.GREEN+"Successfully connected to %s\n! We're good to go!"+Colors.RESET, address)
 			err := conn.Close()
 			if err != nil {
-				return
+				fmt.Printf(Colors.RED+"Some unexpected error happened.\n%s\nWill exit now."+Colors.RESET, err.Error())
+				os.Exit(1)
 			}
 			// Exit with status code 0
 			os.Exit(0)
